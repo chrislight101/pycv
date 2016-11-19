2d8a0a96dadd0422336565b2dedd503ac561b793
CLIGHT <CHRISLIGHT101@GMAIL.COM>
2016-11-19 12:53:02 -0700

    centroid edit

--- centroid.py
+++ centroid.py
@@ -3,7 +3,7 @@ import numpy as np
 
 img = cv2.imread('circle.png',0)
 ret,thresh = cv2.threshold(img,127,255,2)
-contours,hierarchy = cv2.findContours(thresh,1,2)
+contours = cv2.findContours(thresh,1,2)
 
 cnt = contours[0]
 M = cv2.moments(cnt)
@@ -12,9 +12,6 @@ cy = int(M['m01']/M['m00'])
 
 cv2.line(img,(cx-20,cy),(cx+20,cy),50)
 cv2.line(img,(cx,cy-20),(cx,cy+20),50)
-
-epsilon = 0.1*cv2.arcLength(cnt,True)
-approx = cv2.approxPolyDP(cnt,epsilon,True)
 
 cv2.imshow("img",img)
 cv2.waitKey(0)
