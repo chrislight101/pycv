b77be6db513b9215cded66b1d714391bfb2f3de2
CLIGHT <CHRISLIGHT101@GMAIL.COM>
2016-12-18 02:44:01 -0700

    ardreader edits

--- ardreader.py
+++ ardreader.py
@@ -1,8 +1,39 @@
 import serial
 import io
-ser = serial.Serial('/dev/ttyACM1')
-while(1):
-  s = ser.readline()
-  s = s.split()
-  print int(s[0])
-  ser.flush()
+from datetime import datetime, date, time
+import time
+import numpy as np
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+maxread = 1023
+arraysize = 100
+avgsum = 0.0
+avg = 0
+samples = np.zeros(arraysize)
+index = 0
+		
+def sample():
+  global avgsum, index
+  #time.sleep(100.0 / 1000.0)
+  t = str(datetime.time(datetime.now()))
+  s = ser.readline().strip('\0').strip().split()
+  if len(s) != 0 and '\\' not in s:
+    try:
+      s = int((s[0]))
+    except ValueError:
+      sample()
+    if s <= maxread:
+		avgsum -= samples[index]
+		samples[index] = s
+		avgsum += samples[index]
+		index += 1
+		if index > (arraysize - 1):
+		  index = 0
+		avg = avgsum / arraysize
+		print t + "," + str(s) + "," + str(avg)
+
+def simplesample(): 
+  print ser.readline().strip('\0').strip()
+
+while(True):
+  #simplesample()
+  sample()
