ad6ca857fee8a4072f4746eac503aa6aa5fd3685
Chris Light <clight@syncroness.com>
2017-03-18 17:23:19 +0000

    cleanup

--- rem.py
+++ rem.py
@@ -17,13 +17,16 @@ rawCapture = PiRGBArray(camera,size=(h,w))
 
 
 for frame in camera.capture_continuous(rawCapture, format='bgr',use_video_port=True):
-	frame = frame.array
+    frame = frame.array
+    
+    img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    ret,img = cv2.threshold(img,50,255,cv2.THRESH_BINARY)
+    #kernel = np.ones((3,3,np.uint8)
+    img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
 
-
-
-	cv2.imshow('img',frame)
-	cv2.moveWindow('img',0,0)
-	key = cv2.waitKey(1) & 0xFF
-	rawCapture.truncate(0)
-	if key == ord('q'):
-		break
+    cv2.imshow('img',img)
+    #cv2.moveWindow('img',0,0)
+    key = cv2.waitKey(1) & 0xFF
+    rawCapture.truncate(0)
+    if key == ord('q'):
+        break
