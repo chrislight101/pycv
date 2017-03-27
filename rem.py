bd6ccf29a5fc6645841b4db90da863f78b8b54e1
Chris Light <clight@syncroness.com>
2017-03-27 02:13:17 +0000

    changes

--- rem.py
+++ rem.py
@@ -4,17 +4,12 @@ import picamera
 from picamera import PiCamera
 from picamera.array import PiRGBArray
 
-h = 320
-w = 240
-points = []
-kernel = np.ones((3,3),np.uint8)
-model = np.zeros((100,100))
-model[48:52,48:52] = 1
+h = 160
+w = 120
 camera = PiCamera()
 camera.resolution = (h,w)
 camera.framerate = 30
 rawCapture = PiRGBArray(camera,size=(h,w))
-
 
 for frame in camera.capture_continuous(rawCapture, format='bgr',use_video_port=True):
     frame = frame.array
@@ -22,7 +17,7 @@ for frame in camera.capture_continuous(rawCapture, format='bgr',use_video_port=T
     img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     ret,img = cv2.threshold(img,50,255,cv2.THRESH_BINARY)
     #kernel = np.ones((3,3,np.uint8)
-    img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+    #img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
 
     cv2.imshow('img',img)
     #cv2.moveWindow('img',0,0)
