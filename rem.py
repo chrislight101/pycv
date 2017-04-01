5b7704f07cd4df580f738f11fedba9b326e670d3
CLIGHT <CHRISLIGHT101@GMAIL.COM>
2017-04-01 16:26:31 -0600

    changes

--- rem.py
+++ rem.py
@@ -13,6 +13,8 @@ camera.framerate = 30
 rawCapture = PiRGBArray(camera,size=(h,w))
 
 out = cv2.VideoWriter('output.avi',-1,20.0,(320,240))
+timestart = time.time()
+f = open('data.csv','w')
 
 for frame in camera.capture_continuous(rawCapture, format='bgr',use_video_port=True):
     frame = frame.array
@@ -31,6 +33,7 @@ for frame in camera.capture_continuous(rawCapture, format='bgr',use_video_port=T
 
     img = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
     font = cv2.FONT_HERSHEY_SIMPLEX
+    f.write(str(time.time()-timestart) + ',' + str(avg) + '\r\n')
     txt = str(avg)
     cv2.putText(img, txt,(30,30),font,0.9,(0,0,255),2)    
     cv2.imshow('img',img)
@@ -39,6 +42,7 @@ for frame in camera.capture_continuous(rawCapture, format='bgr',use_video_port=T
     rawCapture.truncate(0)
     if key == ord('q'):
         break
-        
+
+f.close()        
 out.release()
 cv2.destroyAllWindows()
--- remtemp.py
+++ remtemp.py
@@ -15,10 +15,11 @@ w = int(cap.get(3))
 cx,cy = int(cap.get(3)/2),int(cap.get(4)/2)
 timeconst = 1491000000
 timestart = time.time()
+f = open('data.csv','w')
 
 while(True):
     ret ,frame = cap.read()
-    frame = cv2.resize(frame,(0,0), fx=2, fy=2)
+    frame = cv2.resize(frame,(0,0), fx=.5, fy=.5)
    
    
    
@@ -38,12 +39,14 @@ while(True):
     img = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
     font = cv2.FONT_HERSHEY_SIMPLEX
     txt = str(timeit.timeit())
+    f.write(str(time.time()-timestart) + ',' + str(avg) + '\r\n')
     cv2.putText(img, txt,(30,30),font,0.9,(0,0,255),2)    
     #display and position window
     cv2.imshow('img',img)
     #cv2.moveWindow('img', 1200,240)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    
+
+f.close()    
 cap.release()
 cv2.destroyAllWindows()
