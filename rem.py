16539ec9bb61eecfcee7e60c378476594e585c0d
CLIGHT <CHRISLIGHT101@GMAIL.COM>
2017-04-01 16:00:19 -0600

    changes

--- rem.py
+++ rem.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import time
 import picamera
 from picamera import PiCamera
 from picamera.array import PiRGBArray
@@ -15,15 +16,23 @@ out = cv2.VideoWriter('output.avi',-1,20.0,(320,240))
 
 for frame in camera.capture_continuous(rawCapture, format='bgr',use_video_port=True):
     frame = frame.array
+    #resize function if needed
+    frame = cv2.resize(frame,(0,0), fx=0.5, fy=0.5)
+    
+    
     
     img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     ret,img = cv2.threshold(img,50,255,cv2.THRESH_BINARY)
-    #kernel = np.ones((3,3,np.uint8)
-    #img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+    avg = np.average(img)
 
-    #frame = cv2.resize(img,(320,240))
-    out.write(img)
-    
+
+
+
+
+    img = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    txt = str(avg)
+    cv2.putText(img, txt,(30,30),font,0.9,(0,0,255),2)    
     cv2.imshow('img',img)
     #cv2.moveWindow('img',0,0)
     key = cv2.waitKey(1) & 0xFF
---
+++ remtemp.py
@@ -0,0 +1,49 @@
+import numpy as np
+import cv2
+import time
+import timeit
+
+#open feed and calculate center
+cap = cv2.VideoCapture(1)
+cap.open(1)
+ret ,frame = cap.read()
+ret ,frame = cap.read()
+ret ,frame = cap.read()
+print(cap.isOpened())
+h = int(cap.get(4))
+w = int(cap.get(3))
+cx,cy = int(cap.get(3)/2),int(cap.get(4)/2)
+timeconst = 1491000000
+timestart = time.time()
+
+while(True):
+    ret ,frame = cap.read()
+    frame = cv2.resize(frame,(0,0), fx=2, fy=2)
+   
+   
+   
+    
+    
+    img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    ret,img = cv2.threshold(img,50,255,cv2.THRESH_BINARY)
+    l_avg, r_avg = np.average(img[0:h,0:cx]), np.average(img[0:h,cx:w])
+    avg = np.average(img)
+ 
+ 
+ 
+ 
+ 
+ 
+    
+    img = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    txt = str(timeit.timeit())
+    cv2.putText(img, txt,(30,30),font,0.9,(0,0,255),2)    
+    #display and position window
+    cv2.imshow('img',img)
+    #cv2.moveWindow('img', 1200,240)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
