acb0bf7ca4ac831daba160174aeea0c36e7c461b
CLIGHT <CHRISLIGHT101@GMAIL.COM>
2017-04-08 16:39:43 -0600

    changes

--- remtemp.py
+++ remtemp.py
@@ -21,6 +21,10 @@ timestart = time.time()
 f = open('data.csv','w')
 font = cv2.FONT_HERSHEY_SIMPLEX
 samples = deque(np.zeros(100))
+thresh = 50
+autothreshold = True
+imwritecounter = 0
+
 
 fourcc = cv2.VideoWriter_fourcc(*'WMV1')
 out = cv2.VideoWriter('vid.avi',fourcc,20.0,(width,height))
@@ -28,13 +32,14 @@ out = cv2.VideoWriter('vid.avi',fourcc,20.0,(width,height))
 while(True):
     ret ,frame = cap.read()
     frame = cv2.resize(frame,(0,0), fx=.5, fy=.5)
-   
-   
-   
+    background = cv2.imread('../frame0.png')
+    background = cv2.cvtColor(background,cv2.COLOR_BGR2GRAY)
     
     
     img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    ret,img = cv2.threshold(img,50,255,cv2.THRESH_BINARY)
+    img = abs(img-background)
+    #img = cv2.GaussianBlur(img,(5,5),0)
+    ret,img = cv2.threshold(img,thresh,255,cv2.THRESH_BINARY)
     #l_avg, r_avg = np.average(img[0:h,0:cx]), np.average(img[0:h,cx:w])
     avg = np.average(img)
     samples.append(avg)
@@ -43,7 +48,17 @@ while(True):
  
     out.write(img)
  
- 
+     #autothreshold    
+    if autothreshold:
+        if avg < 127:
+            thresh = thresh - 2
+        if avg > 127:
+            thresh = thresh + 2
+
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('x'):
+        cv2.imwrite('../frame' + str(imwritecounter) + '.png',frame)
+        imwritecounter = imwritecounter
     
     img = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)  
     txt = str(ma)
@@ -53,7 +68,7 @@ while(True):
     #display and position window
     cv2.imshow('img',img)
     #cv2.moveWindow('img', 1200,240)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if key == ord('q'):
         break
 
 f.close()  
